40eb2fd20945097f0729db0ccab491d80d14a928
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2022-02-11 08:58:07 -0700

    create users_controller with index, show, new, edit, create, update and destroy actions.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,52 @@
+class UsersController < ApplicationController
+    before_action :find_user, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @users = User.all
+    end
+
+    def show
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @user.update(user_pareams)
+            redirect_to user_path(@user)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @user.resources.destroy
+        @user.destroy
+        reset_session
+        redirect_to root_path
+    end
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:username, :password, :email)
+    end
+
+    def find_user
+        @user = User.find(params[:id])
+    end
+end
